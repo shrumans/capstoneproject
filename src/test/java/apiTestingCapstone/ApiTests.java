f72f7eb54fd1772c220736b01c0e0049dd187290
shruthi manjesh <manshranv@gmail.com>
2023-10-20 19:22:47 +0530

    commit capstone files from cmd

---
+++ src/test/java/apiTestingCapstone/ApiTests.java
@@ -0,0 +1,31 @@
+package apiTestingCapstone;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.response.Response;
+
+public class ApiTests {
+	
+	
+	@Test
+	public void getApiValidation() {
+		
+		RestAssured.baseURI="http://localhost:8080/medicare";
+		RestAssured.given().accept(ContentType.HTML).
+		header("Content-Type","text/html;charset=ISO-8859-1").
+		when().get("/show/all/products").
+		then().assertThat().statusCode(200);
+		
+		Response res=RestAssured.given().accept(ContentType.HTML).
+				when().get("/show/all/products");
+		String x=res.getBody().asPrettyString();		
+		Assert.assertTrue(x.contains("Analgesics"));
+	}
+	
+}
---
+++ src/test/java/apiTestingCapstone/BaseApiTest.java
@@ -0,0 +1,11 @@
+package apiTestingCapstone;
+
+import org.testng.annotations.BeforeSuite;
+
+public abstract class BaseApiTest {
+
+	@BeforeSuite
+	public void setup() {
+		
+	}
+}
