8eb86dcead6504dbc6729a585c27c37f7eb4be0f
shruthi manjesh <manshranv@gmail.com>
2023-10-21 11:57:38 +0530

    last commit

--- src/test/java/apiTestingCapstone/ApiTests.java
+++ src/test/java/apiTestingCapstone/ApiTests.java
@@ -14,18 +14,30 @@ public class ApiTests {
 	
 	
 	@Test
-	public void getApiValidation() {
+	public void getApiStatusValidation() {
 		
 		RestAssured.baseURI="http://localhost:8080/medicare";
 		RestAssured.given().accept(ContentType.HTML).
 		header("Content-Type","text/html;charset=ISO-8859-1").
 		when().get("/show/all/products").
 		then().assertThat().statusCode(200);
+	
+	}
+	
+	@Test
+	public void getApiResponseValidation() {
+		RestAssured.baseURI="http://localhost:8080/medicare";
+		RestAssured.given().accept(ContentType.HTML).
+		header("Content-Type","text/html;charset=ISO-8859-1").
+		when().get("/show/category/2/products").
+		then().assertThat().statusCode(200);
 		
 		Response res=RestAssured.given().accept(ContentType.HTML).
-				when().get("/show/all/products");
-		String x=res.getBody().asPrettyString();		
+				when().get("/show/category/2/products");
+		String x=res.getBody().asPrettyString();
 		Assert.assertTrue(x.contains("Analgesics"));
 	}
+	
+	
 	
 }
